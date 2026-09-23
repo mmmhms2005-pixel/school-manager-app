aed56ba2fd5523b7da3278c751981bfddf5c63d1
mmmhms2005-pixel <mmmhms2005@gmail.com>
2026-09-23 19:56:13 +0300

    Update ClassReportScreen.kt

--- app/src/main/java/com/example/schoolmanager/ui/screens/ClassReportScreen.kt
+++ app/src/main/java/com/example/schoolmanager/ui/screens/ClassReportScreen.kt
@@ -1,6 +1,7 @@
 package com.example.schoolmanager.ui.screens
 
 import android.content.Intent
+import androidx.navigation.NavController
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
