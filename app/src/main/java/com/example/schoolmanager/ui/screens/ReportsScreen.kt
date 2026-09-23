25af35347d0e42e01bf5f2d5c9cd2e85158f06e1
mmmhms2005-pixel <mmmhms2005@gmail.com>
2026-09-24 01:10:37 +0300

    Update ReportsScreen.kt

--- app/src/main/java/com/example/schoolmanager/ui/screens/ReportsScreen.kt
+++ app/src/main/java/com/example/schoolmanager/ui/screens/ReportsScreen.kt
@@ -178,7 +178,7 @@ fun ReportsScreen(nav: NavController) {
                         snackbar = snackbarHostState
                     )
     
-                    "monthly_certificate" -> MonthlyCertificateScreen(
+ "monthly_certificate" -> MonthlyCertificateScreen(
     nav = nav,
     dao = dao,
     scope = scope,
