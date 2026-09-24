1475f8b00b68cb09bc295b85a763604917eb23ed
mmmhms2005-pixel <mmmhms2005@gmail.com>
2026-09-24 13:35:51 +0300

    Update PdfGenerator.kt

--- app/src/main/java/com/example/schoolmanager/data/PdfGenerator.kt
+++ app/src/main/java/com/example/schoolmanager/data/PdfGenerator.kt
@@ -27,6 +27,9 @@ object PdfGenerator {
     private const val A4_WIDTH_LS_PT = 842
     private const val A4_HEIGHT_LS_PT = 595
 
+    // أبعاد A5
+private const val A5_WIDTH_PT = 420
+private const val A5_HEIGHT_PT = 595
     private const val MARGIN = 25f
 
     // ═══════════════════════════════════════════
