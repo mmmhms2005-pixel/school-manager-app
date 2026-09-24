8ae1d755517cb929a8d1cff7d4ccc307f18cf2dd
mmmhms2005-pixel <mmmhms2005@gmail.com>
2026-09-24 22:36:24 +0300

    Update AnalyticsScreen.kt

--- app/src/main/java/com/example/schoolmanager/ui/screens/AnalyticsScreen.kt
+++ app/src/main/java/com/example/schoolmanager/ui/screens/AnalyticsScreen.kt
@@ -1,6 +1,7 @@
 package com.example.schoolmanager.ui.screens
 
 import android.content.Intent
+import androidx.navigation.NavController
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -40,6 +41,7 @@ data class Analytics(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AnalyticsScreen(
+    nav: NavController,
     dao: SchoolDao,
     scope: CoroutineScope,
     snackbar: SnackbarHostState
